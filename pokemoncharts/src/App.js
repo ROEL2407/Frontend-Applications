cb28cdcd52ce53e9f8ba319161567833f972474d
ROEL2407 <roel24komen@gmail.com>
2021-11-29 12:09:26 +0100

    updtae: changes for d3 visualisation

--- pokemoncharts/src/App.js
+++ pokemoncharts/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.scss';
 import React from 'react';
 import Fetch from './providers/fetch';
@@ -10,28 +10,31 @@ function App() {
   const [json, setJson] = useState(null);
   // get data async
   useEffect(() => {
-    Fetch().then(data => setJson(data));
-  })
+    Fetch().then(data => {
+      setJson(data)});
+  }, [])
+  
+  if (!json) {
+    return "loading";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <pre>
-          {JSON.stringify(json, null, 3)}
-        </pre>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <BarChart data={json} />
+    <h2>What are the biggest base stats of the gen 1 starter Pokémon?</h2>
+    <div id="filters">
+        <strong>Filter:</strong>
+        
+        <label><input type="radio" name="filters" value="1" id="filter-1-only" class="filters" />First evolution</label>
+        <label><input type="radio" name="filters" value="1" id="filter-2-only" class="filters" />Second
+            evolution</label>
+        <label><input type="radio" name="filters" value="1" id="filter-3-only" class="filters" />Third evolution</label>
+        <label><input type="radio" name="filters" value="1" id="filter-all" class="filters" />All evolutions</label>
+    </div>
+    <div class="hidden" id="toolTip">
+        <p id="type"></p>
+        <p id="value"></p>
+    </div>
+
+      {json && <BarChart data={json} />}
     </div>
   );
 }
