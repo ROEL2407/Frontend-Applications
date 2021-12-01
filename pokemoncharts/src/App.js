f75d59e84c92c32938544379d875cf36c108500a
ROEL2407 <roel24komen@gmail.com>
2021-12-01 12:43:49 +0100

    Update: fixed bar chart not showing

--- pokemoncharts/src/App.js
+++ pokemoncharts/src/App.js
@@ -3,22 +3,8 @@ import './App.scss';
 import React from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
-import Fetch from './providers/fetch';
 import Visualization from "./components/Visualization";
-import { useEffect, useState } from 'react';
-
 function App() {
-  // set default of fetch to null
-  const [json, setJson] = useState(null);
-  // get data async
-  useEffect(() => {
-    Fetch().then(data => {
-      setJson(data)});
-  }, [])
-  
-  if (!json) {
-    return "loading";
-  }
   return (
     <div className="App">
     <div className="nav">
@@ -27,7 +13,7 @@ function App() {
     </div>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/visualization" element={<Visualization />}  data={json}/>
+      <Route path="/visualization" element={<Visualization />}/>
     </Routes>
     </div>
   );
--- pokemoncharts/src/components/Visualization.js
+++ pokemoncharts/src/components/Visualization.js
@@ -1,25 +1,40 @@
 import BarChart from "../charts/BarChart";
+import Fetch from '../providers/fetch';
+import { useEffect, useState } from 'react';
 
+function Visualization () {
 
-export default function Visualization (json) {
-    return (
-        <>
+// set default of fetch to null
+const [json, setJson] = useState(null);
+// get data async
+useEffect(() => {
+  Fetch().then(data => {
+    setJson(data)});
+}, [])
+
+if (!json) {
+  return "loading";
+}
+return (
+    <>
 <h2>What are the biggest base stats of the gen 1 starter Pokémon?</h2>
 <div id="filters">
-    <strong>Filter:</strong>
-    
-    <label><input type="radio" name="filters" value="1" id="filter-1-only" class="filters" />First evolution</label>
-    <label><input type="radio" name="filters" value="1" id="filter-2-only" class="filters" />Second
-        evolution</label>
-    <label><input type="radio" name="filters" value="1" id="filter-3-only" class="filters" />Third evolution</label>
-    <label><input type="radio" name="filters" value="1" id="filter-all" class="filters" />All evolutions</label>
+<strong>Filter:</strong>
+
+<label><input type="radio" name="filters" value="1" id="filter-1-only" class="filters" />First evolution</label>
+<label><input type="radio" name="filters" value="1" id="filter-2-only" class="filters" />Second
+    evolution</label>
+<label><input type="radio" name="filters" value="1" id="filter-3-only" class="filters" />Third evolution</label>
+<label><input type="radio" name="filters" value="1" id="filter-all" class="filters" />All evolutions</label>
 </div>
 <div class="hidden" id="toolTip">
-    <p id="type"></p>
-    <p id="value"></p>
+<p id="type"></p>
+<p id="value"></p>
 </div>
 
-  {json && <BarChart data={json} />}
-  </>
-    );
+{json && <BarChart data={json} />}
+</>
+);
 }
+
+export default Visualization;
